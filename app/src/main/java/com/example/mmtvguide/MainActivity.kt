aed6020090d9af97701bcd4a516d345d46540d76
Adithya Kamath <kamathis4@gmail.com>
2022-05-24 20:37:52 +0530

    ViewPager 2 + Tab layout

--- app/src/main/java/com/example/mmtvguide/MainActivity.kt
+++ app/src/main/java/com/example/mmtvguide/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.mmtvguide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.mmtvguide.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.viewPager.adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+
+        TabLayoutMediator(binding.MainTabLayout, binding.viewPager){tab, pos ->
+            if(pos == 0){
+                tab.text = "Characters"
+            }
+            else if(pos == 1){
+                tab.text = "Locations"
+            }
+        }.attach()
     }
 }
